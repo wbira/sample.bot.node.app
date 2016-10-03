5a0776bbe3a325150120546e0df68369a93852db
Waldemar Bira <waldek.bira@gmail.com>
2016-10-03 20:27:53 +0200

    added rembering name in userData

--- console.js
+++ console.js
@@ -4,11 +4,23 @@ var connector = new builder.ConsoleConnector().listen();
 var bot = new builder.UniversalBot(connector);
 
 bot.dialog('/', [
+    function (session, arg, nextStep) {
+        if (!session.userData.name) {
+            session.beginDialog('/profile');
+        } else {
+            nextStep();
+        }
+    }, function (session) {
+        session.send('Hello %s !', session.userData.name);
+    }]);
+
+bot.dialog('/profile', [
     function (session) {
         builder.Prompts.text(session, 'Hello, what is your name?')
     },
     function (session, results) {
-        session.send('Hello, %s nice to meet you', results.response)
+        session.userData.name = results.response;
+        session.endDialog();
     }
 ]);
 
