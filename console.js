f3f67832dbc4cf1952963dc50634a95e474f8e5d
Waldemar Bira <waldek.bira@gmail.com>
2016-10-04 19:52:01 +0200

    added basic intent dialog for changing name

--- console.js
+++ console.js
@@ -2,8 +2,20 @@ var builder = require('botbuilder');
 
 var connector = new builder.ConsoleConnector().listen();
 var bot = new builder.UniversalBot(connector);
+var intents = new builder.IntentDialog();
 
-bot.dialog('/', [
+bot.dialog('/', intents);
+
+intents.matches(/^change name/i, [
+    function (session) {
+        session.beginDialog('/profile');
+    },
+    function (session, results) {
+        session.send('Ok, Changed your name to %s', session.userData.name)
+    }
+]);
+
+intents.onDefault([
     function (session, arg, nextStep) {
         if (!session.userData.name) {
             session.beginDialog('/profile');
